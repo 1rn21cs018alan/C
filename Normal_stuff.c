57f51f44143214383d7193e4cbd1591fa16f5f0a
Alan George JImcy <117376928+1rn21cs018alan@users.noreply.github.com>
2024-01-25 01:30:21 +0530

    Add files via upload

--- Normal_stuff.c
+++ Normal_stuff.c
@@ -1,6 +1,23 @@
 #include<stdio.h>
 int main(){
     int i=-1;
+    if(1!=0){
+        if(2==0){
+            printf("a");
+        }
+    }
+    else{
+        printf("b");
+    }
+
+    if(1!=0)
+        if(2==0)
+            printf("c");
+    else
+        printf("d");
+
+
+    return 0;
     for(i=0;i<10;i++){
         printf("%d\n",i);
     }
