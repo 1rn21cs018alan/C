8cda221729285fa48714a8570433743c90ed81da
Alan George JImcy <117376928+1rn21cs018alan@users.noreply.github.com>
2024-02-16 16:19:15 +0530

    Add files via upload

--- simple_pattern_printer.c
+++ simple_pattern_printer.c
@@ -1,4 +1,23 @@
 #include<stdio.h>
+int N=10;
+int abs(int x){
+    if (x>0) {
+        return x;
+    }
+    return -x;
+}
+int max(int a,int b){
+    if (a>b){
+        return a;
+    }
+    return b;
+}
+int min(int a,int b){
+    if (a<b){
+        return a;
+    }
+    return b;
+}
 int check(int i,int j){
 
     //line equations(all right angle triange pattern)
@@ -39,18 +58,43 @@ int check(int i,int j){
 
     // if((i-5)*(i-5)+(j-5)*(j-5)>4*4) //circle equation
         // return 0;
-    return 1;
+    // return 1;
+    // if(i==j){
+    //     // if(i%2==0)
+    //         return 1;
+    //     // else
+    //     //     return 0;
+    // }
+    // int x=abs(i-N/2);
+    // int y=abs(j-N/2);
+    // if(i>j){
+    //     if (max(x,y)%2==0){
+    //         return 1;
+    //     }
+
+    // }
+    // else{
+    //     if (max(x,y)%2!=0){
+    //         return 1;
+    //     }
+
+    // }
+    // if((i<j)^(i+j>=N)){
+    // if((i<j)^(i+j>=N)){
+    // if(N-i<=j || i>j){
+        // return 1;
+    // }
+    return 0;
+    // return 1;
 }
 int main(){
-    int N=10;
-    int arr[10][10];
-    for(int i=0;i<10;i++){
-        for(int j=0;j<10;j++){
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
             if(check(i,j)){
-                printf("*");
+                printf("* ");
             }
             else{
-                printf(" ");
+                printf("  ");
             }
         }
         printf("\n");
